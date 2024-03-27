c651955f26b81227ce80d15eb57db6c321e983ae
mferreiradb <mauhmg@gmail.com>
2024-03-27 01:16:36 -0300

    Added port to envs

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,3 +1,7 @@
 import { app } from "./app";
 
-app.listen(3333, () => console.log("Server is running!"));
+const port = process.env.PORT;
+
+app.listen(port, () =>
+  console.log(`Server is running in http://localhost:${[port]}`)
+);
